8acd9c562d9ab6f5c46a96a45f5c0cde022fb0a2
SuperMax <141626084+iamsupermax@users.noreply.github.com>
2024-12-02 23:41:20 +0100

    erty

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+              src="/Lyvam.svg"
+              alt="Lyvam logomark"
+             width={256}
+            height={256}
+          />
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coming soon...
+        </a>
+      </footer>
+    </div>
+  );
+}
