16fd361a8b5eeb062b50ec26fce611c1eb8ea7ff
SuperMax <141626084+iamsupermax@users.noreply.github.com>
2024-12-05 09:48:43 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
-          id="text"
+          className="text"
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           target="_blank"
           rel="noopener noreferrer"
