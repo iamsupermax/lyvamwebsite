0c8d1570264fb0ca6fea11f27b400f267c80120e
SuperMax <141626084+iamsupermax@users.noreply.github.com>
2024-12-30 12:40:49 +0100

    Lundi12h40

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
               src="/Lyvam.svg"
               alt="Lyvam logomark"
              width={1024}
-            height={1024
+            height={1024}
           />
 	<div>
 		<a className="flex items-center gap-2 hover:underline hover:underline-offset-4"
